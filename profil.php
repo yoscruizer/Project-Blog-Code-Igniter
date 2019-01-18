04495c8e15c7c6dba9ad6cfff543355881747dd2
Yosep Alfatah <42576520+yoscruizer@users.noreply.github.com>
2019-01-19 02:07:22 +0700

    File Admin

---
+++ profil.php
@@ -0,0 +1,28 @@
+<?php require_once 'statik_pengunjung.php'; ?>
+<?php require_once 'views/blog/template/header.php'; ?>
+<?php require_once 'views/blog/template/subheader.php'; ?>
+<?php require_once 'views/blog/template/navbar.php'; ?>
+
+<div id="main">
+  <h2>Profil</h2>
+  <p><span class="image right"><img src="uploads/nopic.png" alt="" /></span>
+    <strong>Nama : Yosep alfatah </strong><br>
+    <strong>Nim  : 1617230036</strong><br>
+    <strong>Sekolah  : Smk Negeri 1 Banjar</strong><br>
+    <strong>Jurusan   : Rekayasa Perangkat Lunak</strong><br>
+    <strong>Hobby : Pemrograman,Coding, Internet,Hacking</strong><br><br>
+     Saya telah membuat website blog dengan menggunakan bahasa pemrograman php dan
+     lainnya untuk keperluan meyelesaikan tugas akhir pada matkul pemrograman web .
+     dengan fitur yang terdiri dari :<br>
+     1. Manajemen user bukutamu <br>
+     2. Postingan <br>
+     3. Gallery <br>
+     4. Statik pegunjung <br>
+     5. Manajemen Komentar <br>
+
+     <a href="rajincopas.my.id">Kunjungi Website Rajin Copas</a>
+    <br>
+  </p>
+</div>
+
+<?php require_once 'views/blog/template/footer.php'; ?>
